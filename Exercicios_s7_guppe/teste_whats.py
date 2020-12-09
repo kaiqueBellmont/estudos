ca92a05d8313bcff50c88b7582c5cad1e891c71b
kaique Bellmont <kaiquebellmont@gmail.com>
2020-12-09 11:57:23 -0300

    renomeando pastas

--- Exercicios_s7_guppe/teste_whats.py
+++
@@ -1,4 +0,0 @@
-import pywhatkit as kit
-
-
-kit.sendwhatmsg("+5531993455856","hello", 6,10)
--- Exercicios_s7_guppe/testeskkk.py
+++
@@ -1,8 +0,0 @@
-import pyqrcode
-
-url = pyqrcode.create('https://www.youtube.com/watch?v=J4x_bDAXoDk')
-url.svg('uca-url.svg', scale=8)
-url.eps('uca-url.eps', scale=2)
-print(url.terminal(quiet_zone=1))
-
-
