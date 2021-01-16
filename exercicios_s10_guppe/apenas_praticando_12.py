c9b11a2213dd4d9b16ac672fe35df63763fff95c
kaique Bellmont <kaiquebellmont@gmail.com>
2021-01-16 16:14:11 -0300

    atualizando

--- exercicios_s10_guppe/apenas_praticando_12.py
+++ exercicios_s10_guppe/apenas_praticando_12.py
@@ -9,3 +9,23 @@ def potency(a, b):
 
 
 print(potency('%', 15))
+
+from random import (
+    random,
+    randint,
+    shuffle,
+    choice
+)
+
+lista = [x for x in range(1, 11)]
+lista2 = []
+for x in range(1, 11):
+    n = choice(lista)
+    print(n)
+    lista2.append(n)
+    lista.remove(n)
+
+print(lista)
+print(lista2)
+
+
